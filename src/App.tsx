b84073216e67dc9189190e7cf45bb1f00313a1f4
Joao Gomes <joaogrgomes@gmail.com>
2026-04-19 23:29:56 -0300

    fix: migrate from TanStack Start SSR to plain React Vite SPA for Vercel deploy

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import { Routes, Route } from 'react-router-dom'
+import { Nav } from './components/site/Nav'
+import { Hero } from './components/site/Hero'
+import { About } from './components/site/About'
+import { Services } from './components/site/Services'
+import { Work } from './components/site/Work'
+import { Process } from './components/site/Process'
+import { Clients } from './components/site/Clients'
+import { Cta } from './components/site/Cta'
+import { Footer } from './components/site/Footer'
+
+function HomePage() {
+  return (
+    <>
+      <Nav />
+      <Hero />
+      <About />
+      <Services />
+      <Work />
+      <Process />
+      <Clients />
+      <Cta />
+      <Footer />
+    </>
+  )
+}
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+    </Routes>
+  )
+}
